ff4440a4924f9a7a81b0674af8d4ccf0feb0dfd4
bison <aliteralbison@protonmail.com>
2022-08-27 22:56:43 -0400

    Cleanup and comments

--- gifthing.py
+++ gifthing.py
@@ -12,10 +12,16 @@ from gifmeta.gif import GifStreamException
 
 
 def add_file_arg(parser: argparse.ArgumentParser) -> None:
+    """
+    Add a file argument to an argument parser.
+    """
     parser.add_argument("file", help="The file to operate on.")
 
 
 def open_and_verify_gif(parser: argparse.ArgumentParser, fpath: str) -> gifmeta.Gif:
+    """
+    Open a GIF, verify it, and read all metadata.
+    """
     try:
         return gifmeta.Gif(fpath)
     except GifStreamException as e:
@@ -23,6 +29,9 @@ def open_and_verify_gif(parser: argparse.ArgumentParser, fpath: str) -> gifmeta.
 
 
 def open_and_verify_gifstream(parser: argparse.ArgumentParser, fpath: str) -> GifStream:
+    """
+    Open a GIF, verify it, and place the stream head at the screen descriptor.
+    """
     path = pathlib.Path(fpath)
 
     if not path.exists():
@@ -42,18 +51,31 @@ def open_and_verify_gifstream(parser: argparse.ArgumentParser, fpath: str) -> Gi
 
 
 class ColorGenerator:
+    """
+    The generator for colors. Takes an input color, and randomly transforms it according to
+    instance configuration. At its core, works by generating random offsets and applying adding them
+    to the input colors.
+    """
     def __init__(
         self,
         constant_elems: t.Container[int] = (),
         always_use_first_offset: bool = False
     ) -> None:
+        """
+        Args:
+            constant_elems: Element indices to keep constant during color generation.
+            always_use_first_offset: Instead of generating a new offset every time, generate one
+                offset and use it for every color modified.
+        """
         self.first_offset = (0.0, 0.0, 0.0)
         self.first_offset_set = False
         self.always_use_first_offset = always_use_first_offset
         self.constant_elems = constant_elems
 
-    # Generate a new offset.
     def generate_offset(self) -> t.Tuple[float, float, float]:
+        """
+        Generate a new color offset. All elements will be 0.0 - 1.0.
+        """
         offset = [random.random() if not n in self.constant_elems else 0.0 for n in range(3)]
         offset_t = tuple(offset)
 
@@ -61,19 +83,26 @@ class ColorGenerator:
             self.first_offset = offset_t
             self.first_offset_set = True
 
-        return offset_t # type: ignore
+        return offset_t  # type: ignore
 
-    # Get a new offset, taking into account first offset settings
     def get_next_offset(self) -> t.Tuple[float, float, float]:
+        """
+        Get the next offset according to all generator configuration. Not to be confused with generate_offset(),
+        which always creates a new offset.
+        """
         if self.always_use_first_offset and self.first_offset_set:
             return self.first_offset
 
         return self.generate_offset()
 
-    # Generate a color, taking into account constant_offset
     def generate_color(self, original_color: t.Tuple[float, float, float]) -> t.Tuple[float, float, float]:
+        """
+        Generate a new color based on an input color.
+        """
         offset = self.get_next_offset()
 
+        # Generate color by adding a generated offset to the original color. If this sum exceeds 1.0, it will
+        # wrap back around, starting from 0.0
         new_color = tuple([
             (original + offset) % 1.0 for original, offset in zip(original_color, offset)
         ])
@@ -84,50 +113,80 @@ class ColorGenerator:
         self,
         original_table: t.Iterable[t.Tuple[float, float, float]]
     ) -> t.Sequence[t.Tuple[float, float, float]]:
+        """
+        Convenience function. Generates a full color table using another as input.
+        """
         return [self.generate_color(color) for color in original_table]
 
 
 def gen_rand_rgb_colortable(original_table: t.Iterable[t.Tuple[float, float, float]], generator: ColorGenerator) -> bytes:
+    """
+    Randomize a GIF's color table using the provided color generator.
+    """
     rgb_table = generator.generate_table(original_table)
     return table_float_to_bytes(rgb_table)
 
 
-# Generate the coordinates in HSV, but output RGB.
 def gen_rand_hsv_colortable(original_table: t.Iterable[t.Tuple[float, float, float]], generator: ColorGenerator) -> bytes:
-    # colorsys uses all 0 to 1 for HSV, which is more convenient for us
+    """
+    Convert the color table to HSV, randomize it in that space, then convert back to RGB.
+
+    There are some flaws in the HSV color space (outlined here https://en.wikipedia.org/wiki/HSL_and_HSV#Disadvantages),
+    but it's good enough to mess around with some GIFs. It can produce some interesting effects.
+    """
     hsv_table = generator.generate_table(table_rgb_to_hsv(original_table))
     return table_float_to_bytes(table_hsv_to_rgb(hsv_table))
 
 
 def table_hsv_to_rgb(hsv_table: t.Iterable[t.Tuple[float, float, float]]) -> t.Sequence[t.Tuple[float, float, float]]:
-    # Convert to RGB
+    """
+    Convert a float format table from HSV to RGB space.
+    """
     rgb_table = [colorsys.hsv_to_rgb(*color) for color in hsv_table]
     return rgb_table
 
 
 def table_rgb_to_hsv(rgb_table: t.Iterable[t.Tuple[float, float, float]]) -> t.Sequence[t.Tuple[float, float, float]]:
+    """
+    Convert a float format table from RGB to HSV space.
+    """
     hsv_table = [colorsys.rgb_to_hsv(*color) for color in rgb_table]
     return hsv_table
 
 
 def table_int_to_float(table: t.Iterable[t.Tuple[int, int, int]]) -> t.Sequence[t.Tuple[float, float, float]]:
+    """
+    Convert from a gifmeta format colortable (0-255 ints) to a common format (0.0-1.0 floats).
+    """
     return [(float(x / 255), float(y / 255), float(z / 255)) for x, y, z in table]
 
 
 def table_float_to_bytes(table: t.Iterable[t.Tuple[float, float, float]]) -> bytes:
+    """
+    Convert from the common float table format to bytes for writing back into a GIF.
+    """
     return bytes((int(255 * x) for x in itertools.chain.from_iterable(table)))
 
 
 def mode_randcolor(parser: argparse.ArgumentParser, args: argparse.Namespace) -> None:
-    gif = open_and_verify_gif(parser, args.file)
+    """
+    Randomize the color table of a GIF according to various parameters.
+    """
 
+    # Parse the GIF, and open a stream into it.
+    gif = open_and_verify_gif(parser, args.file)
     gifstream = open_and_verify_gifstream(parser, args.file)
+
+    # Consume screen descriptor and position the gif stream at the global color table.
     screen_desc = gifstream.consume_screen_descriptor()
 
     if not screen_desc.colortable_exists:
         parser.error(f"{args.file}: global color table is not present")
 
+    # Convert to float 0.0 - 1.0 format.
     float_table = table_int_to_float(gif.colortable)
+
+    # Configure color generator.
     hold_set = calc_hold_set(parser, args.hold)
     generator = ColorGenerator(
         constant_elems=hold_set,
@@ -149,6 +208,7 @@ def mode_randcolor(parser: argparse.ArgumentParser, args: argparse.Namespace) ->
     gifstream.close()
 
 
+# Map of characters in --hold string to color element indices.
 CHAR_TO_COLOR_ELEM = {
     "r": 0,
     "g": 1,
@@ -160,6 +220,9 @@ CHAR_TO_COLOR_ELEM = {
 
 
 def calc_hold_set(parser: argparse.ArgumentParser, hold_str: str) -> t.Set[int]:
+    """
+    Implements the conversion between a --hold string and the Set used to control a ColorGenerator instance.
+    """
     charset = set(hold_str.lower())
 
     for c in charset:
@@ -186,7 +249,7 @@ def prepare_argparser() -> argparse.ArgumentParser:
 
     color_mode_group = mode_randcolor_parser.add_mutually_exclusive_group()
     color_mode_group.add_argument(
-        "--rgb", action="store_true", help="Generate using RGB color space."
+        "--rgb", action="store_true", help="Generate using RGB color space. This is the default."
     )
     color_mode_group.add_argument(
         "--hsv", action="store_true", help="Generate using HSV color space"
